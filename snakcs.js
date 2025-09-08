61ecd445c453c23b543d253e735ad14375f38efc
FrancescoMorettiBrs <morettifrancesco.lav@gmail.com>
2025-09-08 13:02:24 +0200

    feat: snack 2

--- snakcs.js
+++ snakcs.js
@@ -19,3 +19,16 @@ console.log("Questa è la somma anonima:", sommaAnonima(4, 7));
 const sommaArrow = (num1, num2) => num1 + num2;
 
 console.log("Questa è la somma arrow", sommaArrow(10, 20));
+
+// 🏆 Snack 2
+// Crea una arrow function che calcola il quadrato di un numero.
+// Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+
+const num = 5;
+const quadrato = (num) => num * num;
+console.log(`Questo è il quadrato di ${num}:`, quadrato(num));
+
+// 🏆 Snack 3
+// Crea una funzione eseguiOperazione
+// Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.
+
