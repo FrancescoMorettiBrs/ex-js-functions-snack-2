15210dcdb0fe1be0fc00d8bc003e94d99d7a251f
FrancescoMorettiBrs <morettifrancesco.lav@gmail.com>
2025-09-09 15:08:57 +0200

    feat: snack 7

--- snakcs.js
+++ snakcs.js
@@ -90,3 +90,23 @@ const creaContatoreAutomatico = (interval, startInterval) => {
 
 const startCounter = creaContatoreAutomatico(1000);
 startCounter();
+
+// 🏆 Snack 7
+// Crea una funzione che ferma un timer dopo un certo tempo
+// Scrivi una funzione eseguiEferma che accetta un messaggio, un tempo di avvio e un tempo di stop. Il messaggio deve essere stampato a intervalli regolari, ma si deve fermare dopo il tempo di stop.
+
+const startTime = 1000;
+const stopTime = 5000;
+const message = "Mi ripeto 5 volte";
+
+const id = setInterval(() => {
+  console.log(message);
+}, startTime);
+
+setTimeout(() => clearInterval(id), stopTime);
+
+function eseguiEferma() {
+  console.log(id);
+}
+
+eseguiEferma(message, startTime, stopTime);
